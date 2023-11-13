03f27cf31685eb258a080918cff5692957d29284
miad-odoo <miad@odoo.com>
2023-11-13 14:55:29 +0100

    can now add multiple sliders

--- carousel_snippet/static/src/snippets/s_snippet_carousel/000.js
+++ carousel_snippet/static/src/snippets/s_snippet_carousel/000.js
@@ -11,28 +11,32 @@ publicWidget.registry.MyCarousel = publicWidget.Widget.extend({
     init() {
         this._super.apply(this, arguments);
         // Initialize the slider (see github doc)
-        tns({
-            mode: 'carousel', // or 'gallery'
-            mouseDrag: true,
-            navPosition: "bottom",
-            gutter: 10,
-            controlsContainer: ".custom_controlsContainer",
-            nextButton: '.prev',
-            nextButton: '.next', // String selector
-            arrowKeys: true, // keyboard support
-            lazyload: false,
-            lazyloadSelector: ".tns-lazy",
-            responsive: {
-                "350": {
-                    "items": 1,
-                    "controls": true,
-                    "edgePadding": 30
+        let sliders = document.querySelectorAll('.slider');
+        for (let i = 0; i < sliders.length ; ++i) {
+            tns({
+                container: sliders[i],
+                mode: 'carousel', // or 'gallery'
+                mouseDrag: true,
+                nav: false,
+                gutter: 10,
+                controlsContainer: ".custom_controlsContainer",
+                nextButton: '.prev',
+                nextButton: '.next', // String selector
+                arrowKeys: true, // keyboard support
+                lazyload: false,
+                lazyloadSelector: ".tns-lazy",
+                responsive: {
+                    "350": {
+                        "items": 1,
+                        "controls": true,
+                        "edgePadding": 30
+                    },
+                    "500": {
+                        "items": 3
+                    }
                 },
-                "500": {
-                    "items": 3
-                }
-            },
-        });
+            });
+        }
     }
 });
 
